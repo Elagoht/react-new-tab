431de6e914f39e455d22dda80fe84e3e314ade51
Furkan Baytekin <furkanbaytekin@gmail.com>
2023-06-22 16:34:25 +0300

    chore: new splash texts

--- src/data/splashTexts.js
+++ src/data/splashTexts.js
@@ -1,19 +1,24 @@
 const splashTexts = [
-  "Do not forget to read physical books.",
-  "You can do it. You work hard!",
-  "Time to watch Youtube? You deserve a break.",
-  "Don't forget to listen music while you study.",
-  "Which one below you'll select?",
-  "Wow! have you ever seen this picture. Oh, yes you have...",
-  "Reddit says you have to see this post.",
-  "Translate again. Did I know right?",
-  "Check your mails. Please. Do it now.",
-  "Netflix and chill.",
-  "Wikipedia has a lot for you.",
-  "Wanna check Instagram?",
-  "You know you can change the search engine, right?",
-  "Let me do it for you!",
-  "Let me guess. Youtube again. Right?",
-];
+  "Embrace the possibilities that lie just one click away.",
+  "Discover the web's hidden gems, one page at a time.",
+  "Let the internet be your guide to endless inspiration.",
+  "Expand your horizons with every new tab.",
+  "Journey through the digital realm and uncover new wonders.",
+  "Unleash your curiosity and let the web surprise you.",
+  "Each new tab holds the potential for a remarkable discovery.",
+  "Refresh your mind, refresh your browser.",
+  "Embrace the unknown, venture into unexplored virtual territories.",
+  "Let the web's symphony of knowledge captivate you.",
+  "Unlock a world of information with every tab you open.",
+  "Indulge in the vastness of the internet's collective wisdom.",
+  "Escape the ordinary and dive into a digital adventure.",
+  "Embark on a digital journey and let serendipity guide you.",
+  "Ignite your imagination with each new tab you explore.",
+  "Surf the web's waves and ride the tide of discovery.",
+  "Expand your perspective and broaden your digital horizons.",
+  "Explore the web's endless corridors and stumble upon hidden treasures.",
+  "Let the internet be your passport to knowledge and inspiration.",
+  "Embrace the ever-evolving landscape of the web."
+]
 
-export default splashTexts;
+export default splashTexts

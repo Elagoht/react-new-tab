05770cc08681f4e227836015856694fa345d5d6a
Furkan Baytekin <furkanbaytekin@gmail.com>
2023-06-25 05:50:51 +0300

    feat: 11 new search engine

--- src/assets/search/index.js
+++ src/assets/search/index.js
@@ -2,12 +2,34 @@ import ddg from "./duckduckgo.png";
 import google from "./google.png";
 import wikipedia from "./wikipedia.png";
 import youtube from "./youtube.png";
+import aol from "./aol.png"
+import ask from "./ask.png"
+import baidu from "./baidu.png"
+import bing from "./bing.webp"
+import ecosia from "./ecosia.png"
+import github from "./github.png"
+import stackoverflow from "./stackoverflow.png"
+import startpage from "./startpage.png"
+import twitter from "./twitter.png"
+import yahoo from "./yahoo.png"
+import yandex from "./yandex.png"
 
 const searchIcons = {
-  ddg: ddg,
-  google: google,
-  wikipedia: wikipedia,
-  youtube: youtube,
-};
+  ddg,
+  google,
+  wikipedia,
+  youtube,
+  aol,
+  ask,
+  baidu,
+  bing,
+  ecosia,
+  github,
+  stackoverflow,
+  startpage,
+  twitter,
+  yahoo,
+  yandex
+}
 
-export default searchIcons;
+export default searchIcons
--- src/data/searchProviders.js
+++ src/data/searchProviders.js
@@ -12,14 +12,69 @@ const searchProviders = [
     link: "https://www.google.com/search?q=",
   },
   {
-    name: "Youtube",
-    icon: searchIcons.youtube,
-    link: "https://www.youtube.com/results?search_query=",
+    name: "Bing",
+    icon: searchIcons.bing,
+    link: "https://www.bing.com/search?q=",
+  },
+  {
+    name: "Startpage",
+    icon: searchIcons.startpage,
+    link: "https://www.startpage.com/do/dsearch?query=",
+  },
+  {
+    name: "Yandex",
+    icon: searchIcons.yandex,
+    link: "https://www.yandex.com/search/?text=",
+  },
+  {
+    name: "Yahoo",
+    icon: searchIcons.yahoo,
+    link: "https://search.yahoo.com/search?p=",
+  },
+  {
+    name: "Ecosia",
+    icon: searchIcons.ecosia,
+    link: "https://www.ecosia.org/search?q=",
+  },
+  {
+    name: "Ask",
+    icon: searchIcons.ask,
+    link: "https://www.ask.com/web?q=",
+  },
+  {
+    name: "Baidu",
+    icon: searchIcons.baidu,
+    link: "https://www.baidu.com/s?wd=",
+  },
+  {
+    name: "AOL",
+    icon: searchIcons.aol,
+    link: "https://search.aol.com/aol/search?q=",
+  },
+  {
+    name: "Stack Overflow",
+    icon: searchIcons.stackoverflow,
+    link: "https://stackoverflow.com/search?q=",
+  },
+  {
+    name: "Github",
+    icon: searchIcons.github,
+    link: "https://github.com/search?q=",
   },
   {
     name: "Wikipedia",
     icon: searchIcons.wikipedia,
     link: "https://en.wikipedia.org/w/index.php?fulltext=1&search=",
+  },
+  {
+    name: "Youtube",
+    icon: searchIcons.youtube,
+    link: "https://www.youtube.com/results?search_query=",
+  },
+  {
+    name: "Twitter",
+    icon: searchIcons.twitter,
+    link: "https://twitter.com/search?q=",
   },
 ];
 
